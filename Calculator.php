7e8128d74af77ccfe1fdcbbbd5e3dbc5a6171961
Nurazem Kaldybaev <nurazemkg@gmail.com>
2015-03-02 17:57:57 +0000

    Add empty method bodies

--- Calculator.php
+++ Calculator.php
@@ -1,3 +1,40 @@
 <?php
+class Calculator {
+    private static $input;
+    private static $operators = array(
+        '+' => 'add',
+        '-' => 'subtract',
+        '*' => 'multiply',
+        '/' => 'divide',
+        '%' => 'modulo'
+    );
+    private static $precedences = array(
+        '+' => 1,
+        '-' => 1,
+        '*' => 2,
+        '/' => 2,
+        '%' => 2,
+    );
+
+    public function evaluate($input) {
+
+    }
+
+    private static function validateInput($input) {
+
+    }
+
+    private static function parseInput($input) {
+
+    }
+
+    private static function convertToPostFix($input) {
+
+    }
+
+    private static function evaluatePostFix($input) {
+
+    }
+}
 
 ?>
